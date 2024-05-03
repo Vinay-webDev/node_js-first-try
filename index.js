01aca2643499d12dca3526bf41e431cc483138e3
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-04 00:02:27 +0530

    something went wrong:(

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ console.log(Multiplication(2,3)); */
 /* how to import modules in CS js */
 const {GenerateRandomNumber, celciusToFahrenheit} = require('./utlis');
 
+import getPosts from './postControll.js';
+
 console.log(`Random number: ${GenerateRandomNumber()}`);
 
 console.log(`celcius to fahreheit: ${celciusToFahrenheit(0)}`);
+
+console.log(getPosts());
 
 
 
